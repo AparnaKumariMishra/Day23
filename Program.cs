d91bcc9042b00f67dda9431d02880eb10e863423
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-03 23:43:24 +0530

    Add files via upload
    Adding UC1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Address_Book;
+
+namespace program
+{
+    class Contact
+    {
+        public static void Main()
+        {
+            UC1Create_Contact AddressBook = new UC1Create_Contact();
+            AddressBook.GetContactDetails();
+            Console.ReadLine();
+        }
+    }
+}
+
