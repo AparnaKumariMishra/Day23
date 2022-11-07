bafbeaeb4b229af68bb459ef13c9a71d09da8252
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 15:46:52 +0530

    Add files via upload
    Added

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_Add_Contact
+{
+    internal class program
+    {
+        class Contact
+        {
+            public static void Main(String[] args)
+            {
+                Create_Contact AddressBook = new Create_Contact();
+                AddressBook.GetContactDetails();
+                AddressBook.Display();
+                Console.ReadLine();
+            }
+        }
+    }
+}
