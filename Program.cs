571756210c115294191a1ae19d16eabcf8563210
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 15:48:23 +0530

    Add files via upload
    Addad

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC4_Remove_Contact
+{
+    internal class program
+    {
+        class Contact
+        {
+            public static void Main(String[] args)
+            {
+                Create_Contact AddressBook = new Create_Contact();
+                AddressBook.AddPerson();
+                AddressBook.Display();
+                AddressBook.RemoveContact();
+                Console.ReadLine();
+            }
+        }
+    }
+}
