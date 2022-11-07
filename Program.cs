ecd4de03360d5d36af3aed4a5f0ecfd11152123b
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 15:51:16 +0530

    Add files via upload
    Added

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC6_Add_Multiple_Addressbook
+{
+    internal class program
+    {
+        class Contact
+        {
+            public static void Main(String[] args)
+            {
+                Create_Contact AddressBook = new Create_Contact();
+                AddressBook.AddMultipleContacts();
+                AddressBook.AddUniqueContacts();
+                AddressBook.DisplayUniqueContacts();
+                Console.ReadLine();
+            }
+        }
+    }
+}
