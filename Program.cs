4000a7e614ce6c6bbfafa449f14a3bc48e8a8435
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 15:47:38 +0530

    Add files via upload
    Added

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC3_Edit_Contact
+{
+    internal class program
+    {
+        class Contact
+        {
+            public static void Main(String[] args)
+            {
+                Create_Contact AddressBook = new Create_Contact();
+                AddressBook.GetContactDetails();
+                AddressBook.Display();
+                AddressBook.edit();
+                Console.ReadLine();
+            }
+        }
+    }
+}
